9f5ba91efe869bbbc36880c7d9d91377012571e8
ajayrnairg <ajayrnairg@gmail.com>
2023-04-18 13:07:31 +0530

    Completed Code

---
+++ wifikeys.h
@@ -0,0 +1,4 @@
+#include <Arduino.h>
+// Change YOUR_AP_NAME and YOUR_AP_PASSWORD to your WiFi credentials
+const char *ssid = "TP-Link_46E5";		   // Put your SSID here
+const char *password = "04955528"; // Put your PASSWORD here
