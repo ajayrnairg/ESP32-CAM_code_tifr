b258a549420b34c6e8e2cb149b4e1d3a54a7855a
AJAY NAIR <55824047+ajayrnairg@users.noreply.github.com>
2023-04-23 02:25:29 +0530

    Update wifikeys.h
    Removed credentials

--- wifikeys.h
+++ wifikeys.h
@@ -1,4 +1,4 @@
 #include <Arduino.h>
 // Change YOUR_AP_NAME and YOUR_AP_PASSWORD to your WiFi credentials
-const char *ssid = "TP-Link_46E5";		   // Put your SSID here
-const char *password = "04955528"; // Put your PASSWORD here
+const char *ssid = "<WIFI_NAME>";		   // Put your SSID here
+const char *password = "<WIFI_PASSWORD>"; // Put your PASSWORD here
